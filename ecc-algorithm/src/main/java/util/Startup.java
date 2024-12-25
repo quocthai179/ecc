3dc5c911748f0513f2a7e7a2d8ac1a941a522742
Thai Huynh <32641155+quocthai179@users.noreply.github.com>
2024-12-25 16:57:29 +0700

    Add files via upload

--- ecc-algorithm/src/main/java/util/Startup.java
+++ ecc-algorithm/src/main/java/util/Startup.java
@@ -7,7 +7,7 @@ import jakarta.enterprise.event.Observes;
 
 import io.quarkus.runtime.LaunchMode;
 import io.quarkus.runtime.StartupEvent;
-import model.Todo;
+import model.EccEntity;
 
 @ApplicationScoped
 public class Startup {
@@ -17,11 +17,11 @@ public class Startup {
     public void start(@Observes StartupEvent evt) {
         // in DEV mode we seed some data
         if(LaunchMode.current() == LaunchMode.DEVELOPMENT) {
-            Todo a = new Todo();
+            EccEntity a = new EccEntity();
             a.task = "First item";
             a.persist();
 
-            Todo b = new Todo();
+            EccEntity b = new EccEntity();
             b.task = "Second item";
             b.completed = new Date();
             b.persist();
